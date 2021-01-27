4aa55ce5d4346829c118209f9ee8c4ee6c7aea67
LVaras <lvaras@aligare.cl>
2021-01-27 14:40:45 -0300

    se agregan tests

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -21,9 +21,9 @@ public class AppTest
     @Before
     public void setUp(){
         System.out.println("Iniciando configuración...");
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://www.amazon.com");
+        //driver.get("https://www.amazon.com");
         driver.manage().window().maximize();
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
@@ -31,12 +31,28 @@ public class AppTest
     }
 
     @Test
-    public void shouldAnswerWithTrue()
+    public void test01()
     {
         System.out.println("Iniciando Pruebas...");
         WebElement searchbox = driver.findElement(By.name("q"));
         searchbox.sendKeys("HandBook Devops");
         searchbox.submit();
-        assertEquals("HandBook Devops", driver.getTitle());
+        assertEquals("HandBook Devops - Buscar con Google", driver.getTitle());
+        WebElement link  = driver.findElement(By.linkText("Imágenes"));
+        link.click();
+        String path="https://www.buscalibre.cl/libro-the-devops-handbook-how-to-create-world-class-agility-reliability-and-security-in-technology-organizations-libro-en-ingles-gene-kim-it-revolution-press/9781942788003/p/48451046";
+        driver.findElement(By.xpath("(//a[contains(@href,'"+path+"')])")).click();
     }
+    
+     @Test
+    public void test02()
+    {
+    	driver.get("https://www.amazon.com");
+        System.out.println("Iniciando Pruebas...");
+        WebElement searchbox = driver.findElement(By.name("field-keywords"));
+        searchbox.sendKeys("The phoenix project");
+        searchbox.submit();
+        assertEquals("HandBook Devops - Buscar con Google", driver.getTitle());
+    }
+   
 }
